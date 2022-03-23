a626453a07d308b1a1d0a67f153be880fcdb35a3
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-03-23 10:08:23 +0100

    while cykly

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
+console.log('První příklad - vzestupná řada 0 - 10 - WHILE');
 
 //TADY NAPIS CYKLUS
 let radaCisel = [];
-for (i = 0; i <=10; i++){
+let i = 0;
+while (i <=10){
   radaCisel.push(i);
+  i++;
 }
 console.log(`${radaCisel}`);
 
@@ -13,27 +15,33 @@ console.log(`${radaCisel}`);
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
-console.log('Druhý příklad - rozdíl mezi <= a <');
+console.log('Druhý příklad - rozdíl mezi <= a < - WHILE');
 
 //TADY NAPIS CYKLUS
 let radaCisel2 = [];
-for (i = 0; i <10; i++){
+i = 0; 
+while (i <10){
   radaCisel2.push(i);
+  i++;
 }
 console.log(`Podmínka s <: ${radaCisel2}`);
 let radaCisel3 = [];
-for (i = 0; i <=10; i++){
+i = 0; 
+while (i <=10){
   radaCisel3.push(i);
+  i++;
 }
 console.log(`Podmínka s <=: ${radaCisel3}`);
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
+console.log('Třetí příklad - sestupná řada 10 - 0 - WHILE');
 
 //TADY NAPIS CYKLUS
 let radaCisel4 = [];
-for (i = 10; i >=0; i = i-1){
+i = 10;
+while (i >=0){
   radaCisel4.push(i);
+  i = i-1;
 }
 console.log(`${radaCisel4}`);
